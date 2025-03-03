84450e0fa0d2054a4e3445960d1b69b5bc0658f5
Jesús Villalba Fernández <jesvilfer@alum.us.es>
2025-03-03 19:18:55 +0100

    Task-011-group-requirement-8

---
+++ src/main/java/acme/entities/review/Review.java
@@ -0,0 +1,62 @@
+
+package acme.entities.review;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidLongText;
+import acme.constraints.ValidScore;
+import acme.constraints.ValidShortText;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Review extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidShortText
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidLongText
+	@Automapped
+	private String				text;
+
+	@Optional
+	@ValidScore
+	@Automapped
+	private String				score;
+
+	@Mandatory
+	@Automapped
+	private boolean				recommended;
+
+}
