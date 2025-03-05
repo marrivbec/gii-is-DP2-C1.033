db5aae657001b3cb0b29344409c88ad3aa1b4f41
angelmanuelferrer <angelfaa19@gmail.com>
2025-03-05 19:09:42 +0100

    feat: :sparkles: derivated-atributes-created-for-flights

---
+++ src/main/java/acme/constraints/ValidFlightNumber.java
@@ -0,0 +1,29 @@
+
+package acme.constraints;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Constraint(validatedBy = {})
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@ReportAsSingleViolation
+
+@NotBlank
+@Pattern(regexp = "^[A-Z]{3}\\d{4}$")
+
+public @interface ValidFlightNumber {
+
+	String message() default "{acme.validation.flight.number.message}";
+
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
--- src/main/java/acme/entities/leg/LegRepository.java
+++ src/main/java/acme/entities/leg/LegRepository.java
@@ -1,6 +1,7 @@
 
 package acme.entities.leg;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,18 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface LegRepository extends AbstractRepository {
 
-	@Query("SELECT l FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledDeparture ASC")
-	List<Leg> findLegsByFlightId(Long flightId);
+	@Query("SELECT l.scheduledDeparture FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledDeparture ASC")
+	List<LocalDateTime> findScheduledDepartureByFlightId(int flightId);
 
+	@Query("SELECT l.scheduledArrival FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledArrival DESC")
+	List<LocalDateTime> findScheduledArrivalByFlightId(int flightId);
+
+	@Query("SELECT l.departureAirport.city FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledDeparture ASC")
+	List<String> findOriginCityByFlightId(int flightId);
+
+	@Query("SELECT l.arrivalAirport.city FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledArrival DESC")
+	List<String> findDestinationCityByFlightId(int flightId);
+
+	@Query("SELECT COUNT(l) - 1 FROM Leg l WHERE l.flight.id = :flightId")
+	Integer findLayoversByFlightId(int flightId);
 }
