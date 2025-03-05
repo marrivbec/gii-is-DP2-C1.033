953ec6bd6f0010928dc3c597ae574931a10a46c3
juanramire <juarammor2@alum.us.es>
2025-03-05 18:40:33 +0100

    feat: :sparkles: Individual requirement 3 student 4

---
+++ src/main/java/acme/entities/assistanceAgent/AssistanceAgent.java
@@ -0,0 +1,77 @@
+
+package acme.entities.assistanceAgent;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidEmployeeCode;
+import acme.constraints.ValidLongList;
+import acme.constraints.ValidLongText;
+import acme.entities.airline.Airline;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AssistanceAgent extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@ValidEmployeeCode
+	@Column(unique = true)
+	@Mandatory
+	private String				employeeCode;
+
+	@ValidLongList
+	@Automapped
+	@Mandatory
+	private List<String>		spokenLanguages;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Optional
+	@ValidLongText
+	@Automapped
+	private String				briefBio;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				salary;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				photo;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@ManyToOne
+	@Mandatory
+	@Automapped
+	private Airline				airline;
+}
