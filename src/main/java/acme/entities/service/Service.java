8a02367a43ff39fd1cec18a60dffe3224a55e19d
juanramire <juarammor2@alum.us.es>
2025-03-11 11:48:16 +0100

    fix: :bug: Airport relationship in service not necessary

--- src/main/java/acme/entities/service/Service.java
+++ src/main/java/acme/entities/service/Service.java
@@ -3,8 +3,6 @@ package acme.entities.service;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
@@ -16,7 +14,6 @@ import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidUrl;
 import acme.constraints.ValidPromoCode;
 import acme.constraints.ValidShortText;
-import acme.entities.airport.Airport;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -59,9 +56,4 @@ public class Service extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-
-	@Mandatory
-	@Valid
-	@ManyToOne
-	private Airport				airport;
 }
