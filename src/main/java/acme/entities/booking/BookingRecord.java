c6d73ff0edbf6dfd0c1e387b236c49d6260ad126
Jesús Villalba Fernández <jesvilfer@alum.us.es>
2025-03-13 17:22:56 +0100

    feat: :fire: CSV solved

--- src/main/java/acme/entities/booking/BookingRecord.java
+++ src/main/java/acme/entities/booking/BookingRecord.java
@@ -1,13 +1,19 @@
 
 package acme.entities.booking;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
 import acme.entities.passenger.Passenger;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class BookingRecord extends AbstractEntity {
 	// Serialisation version --------------------------------------------------
 
