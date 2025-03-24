4ee4e5ac253915468818e35f2c7435ef2a389073
juanramire <juarammor2@alum.us.es>
2025-03-24 19:23:19 +0100

    feat: :zap: First Part Task 11
    List Airports

---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportListService.java
@@ -0,0 +1,60 @@
+/*
+ * AdministratorAirportListService.java
+ *
+ * Copyright (C) 2012-2025 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.administrator.airport;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airport.Airport;
+
+@GuiService
+public class AdministratorAirportListService extends AbstractGuiService<Administrator, Airport> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorAirportRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Airport> airports;
+
+		airports = this.repository.findAllAirports();
+
+		super.getBuffer().addData(airports);
+	}
+
+	//Revisar
+	@Override
+	public void unbind(final Airport airport) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(airport, "name", "city", "emailAddress", "contactPhoneNumber");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportRepository.java
@@ -0,0 +1,32 @@
+/*
+ * AdministratorAirportRepository.java
+ *
+ * Copyright (C) 2012-2025 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.administrator.airport;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.airport.Airport;
+
+@Repository
+public interface AdministratorAirportRepository extends AbstractRepository {
+
+	@Query("select a from Airport a where a.id = :id")
+	Airport findAirportById(int id);
+
+	@Query("select a from Airport a")
+	Collection<Airport> findAllAirports();
+
+}
