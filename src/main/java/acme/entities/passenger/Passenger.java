b400a621abbb2ffad97e542285c54e6439d6efa0
Jesús Villalba Fernández <jesvilfer@alum.us.es>
2025-03-26 16:49:16 +0100

    feat: :fire: edit

--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -4,8 +4,10 @@ package acme.entities.passenger;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -16,6 +18,7 @@ import acme.client.components.validation.ValidMoment;
 import acme.constraints.ValidLongText;
 import acme.constraints.ValidPassportNumber;
 import acme.constraints.ValidShortText;
+import acme.realms.client.Customer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -56,4 +59,9 @@ public class Passenger extends AbstractEntity {
 	private String				specialNeeds;
 
 	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@ManyToOne(optional = false)
+	@Valid
+	private Customer			customer;
 }
