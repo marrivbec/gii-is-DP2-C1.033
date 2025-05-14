e20a698320e283356ecd09124ca16d377f1e17b7
Mario Rivas Becerra <rivasmario013@gmail.com>
2025-05-14 08:54:03 +0200

    Prueba

--- src/main/java/acme/features/technician/maintenanceRecord/TechnicianRecordRepository.java
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianRecordRepository.java
@@ -16,7 +16,8 @@ import acme.entities.task.Task;
 @Repository
 public interface TechnicianRecordRepository extends AbstractRepository {
 
-	//todos los records
+	//todos los records  
+
 	@Query("SELECT f FROM MaintenanceRecord f")
 	Collection<MaintenanceRecord> getAllMaintenanceRecords();
 
